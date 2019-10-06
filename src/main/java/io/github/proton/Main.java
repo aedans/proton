3863abe3db4801611ac7503d39d421bc4d3593f4
Aedan Smith <aedan.smith@gmail.com>
2019-10-05 17:07:01 -0700

    Add TerminalDisplay

--- src/main/java/io/github/proton/Main.java
+++ src/main/java/io/github/proton/Main.java
@@ -1,7 +1,20 @@
 package io.github.proton;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import io.github.proton.display.TerminalDisplay;
+import io.reactivex.rxjava3.core.Observable;
+
 public final class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws Throwable {
+        try (TerminalDisplay display = new TerminalDisplay()) {
+            display.start().blockingAwait();
+            display.write(
+                    new TerminalPosition(0, 0),
+                    Observable.fromArray('H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!').map(TextCharacter::new)
+            ).blockingAwait();
+            display.refresh().blockingAwait();
+            Thread.sleep(Integer.MAX_VALUE);
+        }
     }
 }
---
+++ src/main/java/io/github/proton/display/TerminalDisplay.java
@@ -0,0 +1,55 @@
+package io.github.proton.display;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+
+import java.io.Closeable;
+
+public final class TerminalDisplay implements Closeable {
+    private final TerminalScreen screen = Single.fromSupplier(() -> new DefaultTerminalFactory()
+            .setTerminalEmulatorTitle("Proton")
+            .setPreferTerminalEmulator(false)
+            .createScreen()).blockingGet();
+
+    public TerminalDisplay() {
+
+    }
+
+    public Completable write(TerminalPosition position, TextCharacter character) {
+        return Completable.fromAction(() -> screen.setCharacter(position.getColumn(), position.getRow(), character));
+    }
+
+    public Completable write(TerminalPosition position, Observable<TextCharacter> characters) {
+        return characters.count()
+                .flatMapObservable(count -> Observable.range(0, count.intValue()))
+                .map(position::withRelativeColumn)
+                .zipWith(characters, this::write)
+                .concatMapCompletable(x -> x);
+    }
+
+    public Completable refresh() {
+        return Completable.fromAction(screen::refresh);
+    }
+
+    public Completable clear() {
+        return Completable.fromAction(screen::clear);
+    }
+
+    public Completable start() {
+        return Completable.fromAction(screen::startScreen);
+    }
+
+    public Completable stop() {
+        return Completable.fromAction(screen::stopScreen);
+    }
+
+    @Override
+    public void close() {
+        stop().blockingAwait();
+    }
+}
